fa220213465abf12381ac1d83cda507f86f3dd2e
Dov Salomon <dsalomon@nyu.edu>
2018-03-06 00:08:49 -0500

    added zconfig

---
+++ tag-zsh/oh-my-zsh/custom/func.zsh
@@ -0,0 +1,32 @@
+
+fpath=( "$HOME/.zfunc" "${fpath[@]}" )
+
+autoload -Uz bak
+autoload -Uz bash_greeting
+autoload -Uz cl
+autoload -Uz color_echo
+autoload -Uz count
+autoload -Uz crun
+autoload -Uz ctopdf
+autoload -Uz erun
+autoload -Uz fell
+autoload -Uz fsz
+autoload -Uz linuxsty
+autoload -Uz mkcd
+autoload -Uz ml
+autoload -Uz myip
+autoload -Uz prompt_color
+autoload -Uz prompt_pwd
+autoload -Uz prompt_suffix
+autoload -Uz prompt_time
+autoload -Uz psgrep
+autoload -Uz rmdthis
+autoload -Uz rmspace
+autoload -Uz scriptgen
+autoload -Uz setlock
+autoload -Uz setwall
+autoload -Uz silencetrim
+autoload -Uz spawn
+autoload -Uz spill
+autoload -Uz upgrade
+autoload -Uz vpn
---
+++ tag-zsh/oh-my-zsh/custom/hostssh.zsh
@@ -0,0 +1,6 @@
+
+function hostssh() {
+	if [[ -n "$SSH_CLIENT" ]]; then
+		echo "%m"
+	fi
+}
