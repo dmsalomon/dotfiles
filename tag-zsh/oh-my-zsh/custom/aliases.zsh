7ead1c1360541bcc4e5e2e85eb12eae272dc8c29
Dov Salomon <dsalomon@nyu.edu>
2018-04-12 01:26:36 -0400

    indent linux style alias

--- tag-zsh/oh-my-zsh/custom/aliases.zsh
+++ tag-zsh/oh-my-zsh/custom/aliases.zsh
@@ -15,3 +15,4 @@ alias pm='sudo pacman'
 alias p='cat'
 alias oct='octave --no-gui'
 alias open="open_command"
+alias indentlx="indent -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4 -cli0 -d0 -di1 -nfc1 -i8 -ip0 -l80 -lp -npcs -nprs -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts8 -il1"
