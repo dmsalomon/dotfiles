f9a4ba7bf2dcdc17a39ffa19163eac1aa318de2a
Dov Salomon <dsalomon@nyu.edu>
2018-04-13 00:57:31 -0400

    expand aliases?

--- tag-zsh/oh-my-zsh/custom/aliases.zsh
+++ tag-zsh/oh-my-zsh/custom/aliases.zsh
@@ -16,3 +16,15 @@ alias p='cat'
 alias oct='octave --no-gui'
 alias open='open_command'
 alias indentlx='indent -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4 -cli0 -d0 -di1 -nfc1 -i8 -ip0 -l80 -lp -npcs -nprs -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts8 -il1'
+
+#ealiases=$(alias | grep git | cut -d= -f1 | tr '\n' ' ')
+#function _expand-ealias() {
+#	if [ "$ealiases" \=~ " ${LBUFFER// /} " ]; then
+#		zle _expand_alias
+#		zle expand-word
+#	fi
+#	zle self-insert
+#}
+#zle -N _expand-ealias
+#bindkey ' ' _expand-ealias
+#bindkey '^ ' magic-space
