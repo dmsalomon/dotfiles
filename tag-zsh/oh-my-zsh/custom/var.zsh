f5a49fa60307f26a50fba8f772abf7fb2cbf3d9e
Dov Salomon <dsalomon@nyu.edu>
2018-10-13 23:45:03 -0400

    zsh: zlogin, update ssh vars

--- tag-zsh/oh-my-zsh/custom/var.zsh
+++ tag-zsh/oh-my-zsh/custom/var.zsh
@@ -3,5 +3,5 @@ export EDITOR=vim
 export BROWSER=google-chrome-stable
 export PYTHONDONTWRITEBYTECODE=1
 
-dsx="dovi@dovi.ddns.net"
-dsxi="dovi@192.168.1.46"
+dsx="dms@dovi.ddns.net"
+dsxi="dms@192.168.1.46"
