ba444e5477044ebc7c95af1c006d18188b5223e5
Dov Salomon <dsalomon@nyu.edu>
2019-03-05 00:06:18 -0500

    add functions directly into zsh

--- tag-zsh/oh-my-zsh/custom/func.zsh
+++ tag-zsh/oh-my-zsh/custom/func.zsh
@@ -2,6 +2,38 @@
 fpath+=~/.scripts
 fpath+=~/.zfunc
 
-for f in $HOME/.scripts/*(.x:t); do
-	autoload -Uz "$f"
-done
+function rmdthis() {
+	cd ..
+	rmdir $OLDPWD || cd $OLDPWD
+}
+
+function cl() {
+	cd "$1" && ls
+}
+
+function ml() {
+	last="$@[-1]"
+	mv "$@" && [ -d $last ] && cd $last && ls
+}
+
+function mkcd() {
+	mkdir -p "$1" && cd "$1"
+}
+
+function spill() {
+	if [ $# -eq 0 ]
+	then
+		mv * .. && rmdthis
+	else
+		for dir
+		do
+			d="$(pwd)"
+			cd $dir && mv * $OLDPWD && rmdthis
+			cd "$d"
+		done
+	fi
+}
+
+# for f in $HOME/.scripts/*(.x:t); do
+# 	autoload -Uz "$f"
+# done
