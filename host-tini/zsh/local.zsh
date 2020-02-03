1e1f517cb1f27312b6c24374b93ea5035408a5c6
Dov Salomon <dsalomon@nyu.edu>
2020-02-03 00:19:56 -0500

    bspwm all hosts

--- host-tini/zsh/local.zsh
+++ host-tini/zsh/local.zsh
@@ -1,2 +1,3 @@
 export GOPATH=/home/dms/Documents/go
 export BROWSER="brave"
+export SPOTIFY="LD_PRELOAD=/usr/lib/libcurl.so.3:/usr/lib/spotifywm.so /usr/bin/spotify --force-device-scale-factor=1.8"
